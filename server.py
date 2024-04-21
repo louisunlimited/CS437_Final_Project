f32f37cf4d1b74d453e23864a6dbd27a48143ada
Louis Qian <yuq4@illinois.edu>
2024-04-20 21:57:28 -0500

    feat: basic facial detection

--- server.py
+++ server.py
@@ -1,23 +1,35 @@
 from flask import Flask, Response
-import picamera2
+import picamera2 as pic2
 import cv2
+import numpy as np
 
-app = Flask("123")
-camera = picamera2.Picamera2()
+app = Flask("FinalProject")
+
+# Load the Haar Cascade for face detection
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+
+camera = pic2.Picamera2()
 camera_config = camera.create_preview_configuration()
 camera.configure(camera_config)
-camera.start_preview(picamera2.Preview.NULL)
+camera.start_preview(pic2.Preview.NULL)
 camera.start()
 
 def generate_frames():
     while True:
-        buffer = camera.capture_array()
-        buffer = cv2.cvtColor(buffer, cv2.COLOR_BGR2RGB)
-        _, buffer = cv2.imencode('.jpg', buffer)
+        frame = camera.capture_array()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert to RGB
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
+        
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+        
+        # Draw rectangles 
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+        _, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-           
 
 @app.route("/")
 def hello():
@@ -28,7 +40,5 @@ def video_feed():
     return Response(generate_frames(),
                     mimetype="multipart/x-mixed-replace; boundary=frame")
     
-
-# host: inet address of the server (example: wlan0 -> inet)
 if __name__ == "__main__":
     app.run(host="192.168.1.13", port=5000, debug=False, threaded=True)
