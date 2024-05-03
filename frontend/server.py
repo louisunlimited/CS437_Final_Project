b481bffa9a88076aee236eafd4184c57b6a3e17b
Mike Cheng <minyuc2@illinois.edu>
2024-05-02 22:32:37 -0500

    fix!: showCamera logic

--- frontend/server.py
+++ frontend/server.py
@@ -3,7 +3,7 @@
 ############################################
 #(might do a react/ svelte / vue/ frontend just for fun)
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 from dotenv import load_dotenv
 import os
@@ -12,8 +12,7 @@ app = Flask(__name__)
 
 # List of Raspberry Pi camera streams (IPs of your Raspberry Pis)
 load_dotenv()
-raspi_ip_addr = os.getenv('RASPI_IP')
-rasapi_addresses = [raspi_ip_addr, raspi_ip_addr]
+rasapi_addresses = os.getenv('IP_LIST').split(',')
 streams = [f'http://{raspi_ip_addr}:5000/feed' for raspi_ip_addr in rasapi_addresses]
 webhook_endpoints = [f'http://{raspi_ip_addr}:3000/webhook' for raspi_ip_addr in rasapi_addresses]
 showCameras = [True for raspi_ip_addr in rasapi_addresses]
@@ -27,23 +26,21 @@ def button():
     cameraIndex = int(request.form['camera'])
     webhook_endpoint = webhook_endpoints[cameraIndex]
     if showCameras[cameraIndex]:
-        requests.post(webhook_endpoint, json={'command': 'stop'})
-        print('stop: camera', cameraIndex + 1)
-        showCameras[cameraIndex] = False
+        res = requests.post(webhook_endpoint, json={'command': 'stop'})
+        if res.ok:
+            showCameras[cameraIndex] = False
     else:
-        requests.post(webhook_endpoint, json={'command': 'start'})
-        print('start: camera', cameraIndex + 1)
-        showCameras[cameraIndex] = True
-    print(streams)
-    return render_template('index.html', streams=streams, showCameras=showCameras)
+        res = requests.post(webhook_endpoint, json={'command': 'start'})
+        if res.ok:
+            showCameras[cameraIndex] = True
+    return redirect(url_for('index'))
 
 @app.route('/reboot', methods=['POST'])
 def reboot():
     cameraIndex = int(request.form['camera'])
     webhook_endpoint = webhook_endpoints[cameraIndex]
     requests.post(webhook_endpoint, json={'command': 'reboot'})
-    print('reboot: camera', cameraIndex + 1)
-    return render_template('index.html', streams=streams, showCameras=showCameras)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
